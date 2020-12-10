4541e49e853e93231a7359f954995a7804173b7a
Nyash-Mauro <mauricee423@gmail.com>
2020-12-10 10:04:09 +0300

    first initialization

---
+++ .history/onlineshop/urls_20201210085352.py
@@ -0,0 +1,37 @@
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from shop.views import PostViewset, ProfileViewset
+from django_registration.backends.one_step.views import RegistrationView
+from django.conf import settings
+
+# router = routers.DefaultRouter()
+
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
+    path('', include(router.urls)),
+    path('accounts/register/', RegistrationView.as_view(success_url='/accounts/login/'),
+         include('django_registration.backends.one_step.urls')),
+    path('logout/', auth_views.LogoutView.as_view(),
+         {'next_page': settings.LOGIN_REDIRECT_URL}, name='logout'),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
---
+++ .history/shop/views_20201210090617.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, redirect
+from . models import *
+from .forms import *
+from django.contrib.auth.decorators import login_required
+
+
+
+
+def home(request):
+    Items = Item.objects.all()
+    return render(request, 'award/index.html', {"items": Items})
+
+
+@login_required(login_url='/accounts/login/')
+def new_item(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = itemUpload(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect('home')
+    else:
+        form = itemUpload()
+        return render(request, 'shop/new_item.html', {"form": form})
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password1 = form.cleaned_data['password1']
+            recipient = User(username=username, email=email)
+            try:
+                (request, f'Account has been created successfully!')
+            except:
+                print('error')
+            return redirect('login')
+
+    else:
+        form = RegisterForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'registration/registration_form.html', context)
+
+
+def search_project(request):
+
+    if 'search' in request.GET and request.GET["search"]:
+
+        search_term = request.GET.get("search")
+        searched_project = Item.objects.filter(title__icontains=search_term)
+        message = f"{search_term}"
+        return render(request, 'award/search.html', {"message": message, "projects": searched_project})
+
+    else:
+        message = "You haven't searched for any term "
+        return render(request, 'award/search.html', {"message": message})
+
+
+@login_required(login_url='/accounts/profile/')
+def update_profile(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST":
+        form = UpdateProfileForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = UpdateProfileForm(instance=request.user.profile)
+        return render(request, 'award/update_profile.html', {'form': form})
+
+
+def profile_info(request):
+
+    current_user = request.user
+    profile_info = Profile.objects.filter(user=current_user).first()
+    projects = request.user.post_set.all()
+
+    return render(request, 'award/profile.html', {"projects": projects, "profile": profile_info, "current_user": current_user})
+
+
